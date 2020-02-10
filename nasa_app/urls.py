d1cf08ce3f2ae311133dc3bb7234dd2185fa8239
hasan.celik <hasan.celik@etiya.com>
2020-02-10 10:52:59 +0300

    comments removed

--- nasa_app/urls.py
+++ nasa_app/urls.py
@@ -13,25 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.contrib.auth import login, views
-from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.views import LoginView
-from django.urls import path, include, reverse
-from django.utils.functional import lazy
-from django.views.generic import RedirectView
 
-# from dashboard.management.views import LoginView
+from django.views.generic import RedirectView
 from django.contrib import admin
 from django.urls import path, include
 
-# login_forbidden =  user_passes_test(lambda u: u.is_anonymous(), '/')
-# from dashboard.management.forms import LoginForm
-
-# login_forbidden = user_passes_test(lambda u: u.is_anonymous(), lazy(reverse, str)('home_apod'))
-
 urlpatterns = [
-    path('', RedirectView.as_view(pattern_name='login', permanent=False)),         #SESSION CONTROL
+    path('', RedirectView.as_view(pattern_name='login', permanent=False)),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
